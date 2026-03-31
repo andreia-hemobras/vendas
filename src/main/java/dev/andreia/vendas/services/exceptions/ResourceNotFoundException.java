d72b1322bbf8db410b6d72637f31ce596261bd52
andreia-hemobras <andreia.hemobras@gmail.com>
2026-03-31 14:32:38 -0300

    Exception handling - findById

---
+++ src/main/java/dev/andreia/vendas/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.andreia.vendas.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
